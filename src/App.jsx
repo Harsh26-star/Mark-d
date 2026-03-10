4c2084ebbe49d99650256b4397a5c285c7cb07b1
Harsh <harsh26sagathia@gmail.com>
2026-03-10 19:01:25 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import { supabase } from './lib/supabaseClient'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  console.log(supabase)
+
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default App
