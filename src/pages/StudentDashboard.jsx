d9cb527e49f5c24f6f0a6dd104ab5d833ce2b78d
Harsh <harsh26sagathia@gmail.com>
2026-04-04 16:10:53 +0530

    feat: add otp functionality

--- src/pages/StudentDashboard.jsx
+++ src/pages/StudentDashboard.jsx
@@ -1,27 +1,75 @@
 import React, { useState } from 'react'
 import QRScanner from '../components/QRScanner'
+import { supabase } from '../lib/supabaseClient'
 
 function StudentDashboard() {
 
-  const [selectedMode, setSelectedMode] = useState('')
+  const [mode, setMode] = useState(null)
+  const [otp, setOtp] = useState('')
+  const [error, setError] = useState(null)
+  const [success, setSuccess] = useState(null)
 
-  function handleSelectMode() {
-    
+
+  async function handleOTPSubmit(e) {
+    e.preventDefault()
+
+    // Find session with this OTP
+    const { data: session } = await supabase
+      .from('sessions')
+      .select('*')
+      .eq('otp', otp)
+      .gt('closes_at', new Date().toISOString())
+      .single()
+
+    if (!session) {
+      setError('Invalid or expired OTP')
+      return
+    }
+
+    // Get current student
+    const { data: { user } } = await supabase.auth.getUser()
+
+    // Check if already marked
+    const { data: existing } = await supabase
+      .from('attendance')
+      .select('*')
+      .eq('session_id', session.id)
+      .eq('student_id', user.id)
+      .single()
+
+    if (existing) {
+      setError('Already marked present')
+      return
+    }
+
+    // Mark attendance
+    const { error } = await supabase
+      .from('attendance')
+      .insert({
+        session_id: session.id,
+        student_id: user.id,
+        status: true
+      })
+
+    if (!error) setSuccess('Attendance marked sucessfully!')
   }
 
   return (
     <div>
-      <form onSubmit={handleSelectMode}>
-        <label>Select Mode</label>
-        <select id="mode"
-          onChange={(e) => setSelectedMode(e.target.value)}
-        >
-          <option value="QR">Qr</option>
-          <option value="OTP">OTP</option>
-        </select>
-        <button type='submit'>Confirm Mode</button>
-      </form>
-      
+      <button onClick={() => setMode("qr")}>Scan QR</button>
+      <button onClick={() => setMode("otp")}>Enter OTP</button>
+
+      {mode === "otp" && (
+        <form onSubmit={handleOTPSubmit}>
+          <input
+            onChange={(e) => { setOtp(e.target.value) }}
+            type="text" placeholder='Enter OTP' />
+          <button type='submit'>Submit</button>
+        </form>
+      )}
+      {mode === "qr" && (
+        <QRScanner />
+      )}
     </div>
   )
 }
