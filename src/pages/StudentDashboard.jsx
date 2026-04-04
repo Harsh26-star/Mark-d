2c81e3fbd7d6f53912abfc277bf5ed446fb8d3dd
Harsh <harsh26sagathia@gmail.com>
2026-04-04 14:05:02 +0530

    feat: add mode selection for students

--- src/pages/StudentDashboard.jsx
+++ src/pages/StudentDashboard.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import QRScanner from '../components/QRScanner'
 
 function StudentDashboard() {
 
+  const [selectedMode, setSelectedMode] = useState('')
+
+  function handleSelectMode() {
+    
+  }
+
   return (
     <div>
-      <h1>QR Scanner</h1>
-      <QRScanner />
+      <form onSubmit={handleSelectMode}>
+        <label>Select Mode</label>
+        <select id="mode"
+          onChange={(e) => setSelectedMode(e.target.value)}
+        >
+          <option value="QR">Qr</option>
+          <option value="OTP">OTP</option>
+        </select>
+        <button type='submit'>Confirm Mode</button>
+      </form>
+      
     </div>
   )
 }
